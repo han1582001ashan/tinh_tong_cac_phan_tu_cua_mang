4f86757894368e3b25e98d2622b67e10298502a8
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2024-08-19 20:21:08 +0700

    nopbai

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace DisplayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+            int sum = 0;
+            for(int i=0; i<numbers.Length; i++){
+                sum = sum + numbers[i];
+            }
+            Console.Write("Sum = "+ sum);
+        }
+    }
+}
+
